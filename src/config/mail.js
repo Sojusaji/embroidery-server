537b80b5c1193451951346191921d1f2e8fd97a9
Soju Saji <sojusaji.20@gmail.com>
2026-07-07 20:03:07 +0530

    feat(mail): implement OAuth2 Gmail sending

--- src/config/mail.js
+++ src/config/mail.js
@@ -2,17 +2,29 @@ import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 dotenv.config();
 
-if (!process.env.GMAIL || !process.env.PASSWORD) {
+if (!process.env.GMAIL
+    || !process.env.GOOGLE_CLIENT_ID_FORMAIL
+    || !process.env.GOOGLE_CLIENT_SECRET_FORMAIL
+    || !process.env.REFRESH_TOKEN_FORMAIL) {
     console.error("❌ Crucial environment variables missing inside your .env configuration file.");
 }
 
 
+
 export const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
+        type: "OAuth2",
         user: process.env.GMAIL,
-        pass: process.env.PASSWORD, 
-    }
+        clientId: process.env.GOOGLE_CLIENT_ID_FORMAIL,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET_FORMAIL,
+        refreshToken: process.env.REFRESH_TOKEN_FORMAIL,
+    },
+    pool: true,
+    maxConnections: 3,
+    maxMessages: 100
 });
 
 
