19c743087ac0d075663064a348314ad35f76e09b
Soju Saji <sojusaji.20@gmail.com>
2026-07-28 17:44:16 +0530

    feat(validate): add query and param validation to validate  middleware

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -2,18 +2,38 @@ import AppError from '../utils/appError.js';
 
 export const validate = (schema) => {
     return async (req, res, next) => {
+
         try {
-            const { name, email } = req.body;
-            console.log("name and email",name,email);
-            req.body = await schema.validateAsync(req.body, {
-                abortEarly: false,
-                stripUnknown: true
-            });
+
+            const dataToValidate = {
+                params: req.params,
+                body: req.body,
+                query: req.query,
+            };
+
+            const value = await schema.validateAsync(dataToValidate,
+                {
+                    abortEarly: false,
+                    stripUnknown: true
+                });
+            if (value.body) req.body = value.body;
+            if (value.params) req.params = value.params;
+
+
+            if (value.query) {
+                Object.keys(req.query).forEach((key) => delete req.query[key]); 
+                Object.assign(req.query, value.query);
+            }
+           
             return next();
         } catch (error) {
+            console.log('error occuired :', error);
+            if (error.isJoi || error.details) {
+                const errorMessage = error.details.map((el) => el.message).join(', ');
+                return next(new AppError(errorMessage, 400));
+            }
 
-            const errorMessage = error.details.map(el => el.message).join(', ');
-            return next(new AppError(errorMessage, 400));
+            return next(error);
         }
     };
 };
